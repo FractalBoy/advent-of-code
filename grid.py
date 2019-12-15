7fa92869ce57aad7721d1491ab3acdf066c07ee5
Marc Reisner <reisner.marc@gmail.com>
2019-12-14 23:09:36 -0600

    simplify, fix following algorithm

--- grid.py
+++ grid.py
@@ -7,26 +7,29 @@ from collections import defaultdict
 
 
 def main():
-    grid = Grid((1, 1))
-    for line in fileinput.input():
-        if len(line):
-            grid.place_wire(line.strip())
+    lines = [line.strip() for line in fileinput.input()]
+    grid = Grid((1, 1), lines)
     #print(grid)
-    print(grid.find_shortest_taxicab_distance_to_intersection())
+    #print(grid.find_shortest_taxicab_distance_to_intersection())
     print(grid.find_shortest_path_distance_to_intersection())
 
 
 class Grid:
-    def __init__(self, central_port):
+    def __init__(self, central_port, wire_specs):
         self._grid = defaultdict(lambda: Port())
         self._grid[central_port] = Port(is_central=True)
         self._central_port = central_port
         self._current_loc = central_port
         self._current_wire = 0
         self._wires = set()
+        self._intersections = defaultdict(dict)
+        self._current_length = 0
+        self._instructions = None
+        self._wire_specs = wire_specs
+        self.place_wires()
 
     def find_intersections(self):
-        return [point for point in self._grid.keys() if self._grid[point].is_intersection()]
+        return [point for point in self._grid if self._grid[point].is_intersection()]
 
     def find_shortest_taxicab_distance_to_intersection(self):
         return min([self.calculate_taxicab_distance_from_central_port(x) for x in self.find_intersections()])
@@ -35,62 +38,32 @@ class Grid:
         return abs(self._central_port[0] - point[0]) + abs(self._central_port[1] - point[1])
 
     def find_shortest_path_distance_to_intersection(self):
-        intersections = {}
-        for wire in self._wires:
-            _, intersections[wire] = self.follow(wire)
-
-        lengths = [[l for l in intersections[key].values()]
-                   for key in intersections.keys()]
+        self.place_wires(follow_only=True)
+        lengths = [[self._intersections[key][point] for point in sorted(self._intersections[key].keys())]
+                   for key in self._intersections]
         return min([sum(l) for l in zip(*lengths)])
 
-    def follow(self, wire, length=0, prev_loc=None, curr_loc=None, visited=set(), intersections={}):
-        if curr_loc is None:
-            curr_loc = self._central_port
-
-        # Possible next moves are found by looking at all non-diagonal next points
-        possible_next_moves = {(curr_loc[0], curr_loc[1] - 1),
-                               (curr_loc[0], curr_loc[1] + 1),
-                               (curr_loc[0] - 1, curr_loc[1]),
-                               (curr_loc[0] + 1, curr_loc[1])}
-
-        # Avoid going back the spot we just came from
-        if prev_loc is not None:
-            possible_next_moves.remove(prev_loc)
-
-        # If we're at a point that intersects with ourselves, we need to make sure
-        # not to go back to any previously visited points, otherwise we'll go in circles
-        if self._grid[curr_loc].is_self_intersection(wire):
-            possible_next_moves -= visited
-
-        for move in possible_next_moves:
-            if self._grid[move].has_wire(wire):
-                # Keep track of visited nodes, increase the length the first time we reach a node
-                if move not in visited:
-                    length = length + 1
-                    if self._grid[move].is_intersection():
-                        intersections[move] = length
-                    visited.add(move)
-                length, _ = self.follow(wire, length=length,
-                                        prev_loc=curr_loc, curr_loc=move, visited=visited, intersections=intersections)
-
-        return length, intersections
-
-    def place_wire(self, wire_spec):
-        self._current_loc = self._central_port
+    def place_wires(self, follow_only=False):
         dispatch_table = {
-            'U': lambda spots: self._go_up(spots),
-            'D': lambda spots: self._go_down(spots),
-            'L': lambda spots: self._go_left(spots),
-            'R': lambda spots: self._go_right(spots)
+            'U': self._go_up,
+            'D': self._go_down,
+            'L': self._go_left,
+            'R': self._go_right
         }
 
-        self._current_wire += 1
-        self._wires.add(self._current_wire)
-        instructions = wire_spec.strip().split(',')
-        for instruction in instructions:
-            dir = instruction[0]
-            spots = int(instruction[1:])
-            dispatch_table[dir](spots)
+        self._current_wire = 0
+
+        for wire_spec in self._wire_specs:
+            self._current_loc = self._central_port
+            self._current_wire += 1
+            self._current_length = 0
+            if not follow_only:
+                self._wires.add(self._current_wire)
+            instructions = wire_spec.split(',')
+            for instruction in instructions:
+                dir = instruction[0]
+                spots = int(instruction[1:])
+                dispatch_table[dir](spots, follow_only)
 
     def _place_node(self, point, direction):
         self._grid[point].place(self._current_wire, direction)
@@ -102,36 +75,40 @@ class Grid:
             for y in range(min(ys), max(ys) + 1):
                 self._grid[x, y]
 
-    def _go_left(self, spots):
-        self._go_horizontal(-spots)
+    def _go_left(self, spots, follow_only=False):
+        self._go_horizontal(-spots, follow_only)
 
-    def _go_right(self, spots):
-        self._go_horizontal(spots)
+    def _go_right(self, spots, follow_only=False):
+        self._go_horizontal(spots, follow_only)
 
-    def _go_up(self, spots):
-        self._go_vertical(spots)
+    def _go_up(self, spots, follow_only=False):
+        self._go_vertical(spots, follow_only)
 
-    def _go_down(self, spots):
-        self._go_vertical(-spots)
+    def _go_down(self, spots, follow_only=False):
+        self._go_vertical(-spots, follow_only)
 
-    def _go_horizontal(self, spots):
+    def _go_horizontal(self, spots, follow_only=False):
         direction = int(spots / abs(spots))
-        for _ in range(0, abs(spots) + 1):
-            self._place_node(self._current_loc, 'H')
+        for _ in range(0, abs(spots)):
+            if not follow_only:
+                self._place_node(self._current_loc, 'H')
             self._current_loc = (
                 self._current_loc[0] + direction, self._current_loc[1])
+            self._current_length += 1
+            if self._grid[self._current_loc].is_intersection():
+                self._intersections[self._current_wire][self._current_loc] = self._current_length
 
-        self._current_loc = (self._current_loc[0] - direction, self._current_loc[1])
-
-    def _go_vertical(self, spots):
+    def _go_vertical(self, spots, follow_only=False):
         direction = int(spots / abs(spots))
-        for _ in range(0, abs(spots) + 1):
-            self._place_node(self._current_loc, 'V')
+        for _ in range(0, abs(spots)):
+            if not follow_only:
+                self._place_node(self._current_loc, 'V')
             self._current_loc = (
                 self._current_loc[0], self._current_loc[1] + direction)
-        
-        self._current_loc = (self._current_loc[0], self._current_loc[1] - direction)
-
+            self._current_length += 1
+            if self._grid[self._current_loc].is_intersection():
+                self._intersections[self._current_wire][self._current_loc] = self._current_length
+    
     def __repr__(self):
         self._fix_grid()
         repr = ''
