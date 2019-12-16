af72e1b28fa084680b09b7be08c3640f45f843a5
Marc Reisner <reisner.marc@gmail.com>
2019-12-15 19:32:02 -0600

    finish day 5

--- opcodes.py
+++ opcodes.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 
-import fileinput
 from functools import reduce, partial
-import operator
+import fileinput
+import os
 
+DEBUG = 'DEBUG' in os.environ and os.environ['DEBUG']
 
 def main():
     opcodes = []
 
-    with open('day5.txt', 'r') as f:
-        for line in f.readlines():
-            line = line.strip()
-            opcodes.extend(line.split(','))
+    for line in fileinput.input():
+        line = line.strip()
+        if len(line) == 0:
+            break
+        opcodes.extend(line.split(','))
 
     computer = IntCodeComputer(opcodes, partial(input, 'Enter input: '), print)
     computer.run()
@@ -22,6 +24,7 @@ class IntCodeComputer:
         self._opcodes = opcodes
         self._input = inp
         self._output = outp
+        self._pos = 0
 
     def run(self):
         dispatch_table = {
@@ -29,12 +32,19 @@ class IntCodeComputer:
             2: MultiplyOperation,
             3: SaveOperation,
             4: LoadOperation,
+            5: JumpIfTrueOperation,
+            6: JumpIfFalseOperation,
+            7: LessThanOperation,
+            8: EqualsOperation,
             99: ExitOperation
         }
 
-        pos = 0
+        self._pos = 0
 
         while True:
+            if DEBUG:
+                print(self._opcodes)
+            pos = self._pos
             opcode = int(self._opcodes[pos][-2:].lstrip('0'))
 
             if opcode in dispatch_table:
@@ -44,17 +54,22 @@ class IntCodeComputer:
 
             num_parameters = operation.num_parameters()
             parameters = tuple(self._opcodes[pos + 1:pos + num_parameters + 1])
-            parameter_modes = self._opcodes[pos][:-2].rjust(num_parameters, '0')
-            parameter_modes = tuple(reversed(parameter_modes))
-            parameters = zip(parameters, parameter_modes)
+            modes = self._opcodes[pos][:-2].rjust(num_parameters, '0')
+            modes = tuple(reversed(modes))
+            parameters = zip(parameters, modes)
             parameters = self.get_parameters(parameters)
-            operation = operation(parameters, self._input, self._output)
+            operation = operation(parameters,
+                                  self._input,
+                                  self._output,
+                                  self.set_instruction_pointer)
 
             if operation.should_exit:
                 break
-
+            
             operation.perform_operation()
-            pos += operation.increment_by()
+
+            if not operation.modified_instruction_pointer:
+                self._pos += operation.increment_by()
 
     def get_parameters(self, parameters):
         dispatch_table = {
@@ -71,6 +86,8 @@ class IntCodeComputer:
         if value is None:
             return self._opcodes[parameter]
         else:
+            if DEBUG:
+                print(f'placing {value} into position {parameter}')
             self._opcodes[parameter] = str(value)
 
     def immediate_mode(self, parameter, value=None):
@@ -78,13 +95,21 @@ class IntCodeComputer:
             raise Exception('cannot set value in immediate mode')
         return parameter
 
+    def set_instruction_pointer(self, pos):
+        if DEBUG:
+            print(f'setting instruction pointer to {pos}')
+        self._pos = int(pos)
+
 
 class Operation:
-    def __init__(self, parameters, inp, outp):
+    def __init__(self, parameters, inp, outp, set_instruction_pointer):
         self.parameters = parameters
+        self.should_exit = False
+        self.modified_instruction_pointer = False
+
         self._input = inp
         self._output = outp
-        self.should_exit = False
+        self._set_instruction_pointer = set_instruction_pointer
 
     @classmethod
     def num_parameters(cls):
@@ -94,7 +119,7 @@ class Operation:
     def increment_by(cls):
         return cls.num_parameters() + 1
 
-    def perform_operation(self, parameters, inp, outp):
+    def perform_operation(self):
         pass
 
 
@@ -105,6 +130,8 @@ class AddOperation(Operation):
 
     def perform_operation(self):
         operand1, operand2, target = self.parameters
+        if DEBUG:
+            print(f'adding {operand1()} and {operand2()}')
         target(int(operand1()) + int(operand2()))
 
 
@@ -115,6 +142,8 @@ class MultiplyOperation(Operation):
 
     def perform_operation(self):
         operand1, operand2, target = self.parameters
+        if DEBUG:
+            print(f'multiplying {operand1()} and {operand2()}')
         target(int(operand1()) * int(operand2()))
 
 
@@ -125,6 +154,8 @@ class SaveOperation(Operation):
 
     def perform_operation(self):
         target, = self.parameters
+        if DEBUG:
+            print(f'getting input')
         target(self._input())
 
 
@@ -135,13 +166,75 @@ class LoadOperation(Operation):
 
     def perform_operation(self):
         target, = self.parameters
+        if DEBUG:
+            print(f'outputting {target()}')
         self._output(target())
 
 
 class ExitOperation(Operation):
-    def __init__(self, parameters, inp, outp):
-        super().__init__(parameters, inp, outp)
+    def __init__(self, *params):
+        super().__init__(*params)
         self.should_exit = True
+
+
+class JumpOperation(Operation):
+    @classmethod
+    def num_parameters(cls):
+        return 2
+
+    def evaluate(self, value):
+        pass
+
+    def perform_operation(self):
+        self.modified_instruction_pointer = False
+        value, pos = self.parameters
+        if self.evaluate(value()):
+            self._set_instruction_pointer(pos())
+            self.modified_instruction_pointer = True
+
+
+class JumpIfTrueOperation(JumpOperation):
+    def evaluate(self, value):
+        if DEBUG:
+            print(f'evaluating {value} != 0')
+        return int(value) != 0
+
+
+class JumpIfFalseOperation(JumpOperation):
+    def evaluate(self, value):
+        if DEBUG:
+            print(f'evaluating {value} == 0')
+        return int(value) == 0
+
+
+class CompareOperation(Operation):
+    @classmethod
+    def num_parameters(cls):
+        return 3
+
+    def evaluate(self, value1, value2):
+        pass
+
+    def perform_operation(self):
+        value1, value2, pos = self.parameters
+        if self.evaluate(value1(), value2()):
+            pos(1)
+        else:
+            pos(0)
+
+
+class LessThanOperation(CompareOperation):
+    def evaluate(self, value1, value2):
+        if DEBUG:
+            print(f'evaluating {value1} < {value2}')
+        return int(value1) < int(value2)
+
+
+class EqualsOperation(CompareOperation):
+    def evaluate(self, value1, value2):
+        if DEBUG:
+            print(f'evaluating {value1} == {value2}')
+        return int(value1) == int(value2)
 
 
 if __name__ == '__main__':
