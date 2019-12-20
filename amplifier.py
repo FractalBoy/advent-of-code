a00e01696d5037699c43dc92676910bb674623dd
Marc Reisner <reisner.marc@gmail.com>
2019-12-19 21:35:56 -0600

    finish day 7

--- amplifier.py
+++ amplifier.py
@@ -2,38 +2,64 @@
 
 from functools import partial
 from intcode import IntCodeComputer
+from itertools import permutations
 import fileinput
 import os
 
 DEBUG = 'DEBUG' in os.environ and os.environ['DEBUG']
 
+output_value = None
+
+
 def main():
     for line in fileinput.input():
         opcodes = line.strip().split(',')
         break
-    amp = AmplificationCircuit(opcodes)
-    amp.run()
+
+    output_dict = {}
+
+    for phase_settings in permutations(range(5)):
+        amp = AmplificationCircuit(opcodes, phase_settings, partial(
+            output, output_dict, phase_settings))
+        amp.run()
+
+    output_dict = {k: v for k, v in reversed(sorted(output_dict.items(), key=lambda item: item[1]))}
+    print(list(output_dict.items())[0])
+
+
+def output(output_dict, permutation, value):
+    output_dict[permutation] = int(value)
 
 
 class AmplificationCircuit:
-    def __init__(self, opcodes):
+    def __init__(self, opcodes, phase_settings, output):
         self.pipe = None
         self.input_number = 0
+        self.phase_settings = phase_settings
+        self.phase_index = 0
         self.amplifiers = [
-            IntCodeComputer(opcodes, partial(
-                input, 'Enter input: '), self.send_to_pipe),
+            IntCodeComputer(opcodes, self.initial_input, self.send_to_pipe),
             IntCodeComputer(opcodes, self.recv_from_pipe, self.send_to_pipe),
             IntCodeComputer(opcodes, self.recv_from_pipe, self.send_to_pipe),
             IntCodeComputer(opcodes, self.recv_from_pipe, self.send_to_pipe),
-            IntCodeComputer(opcodes, self.recv_from_pipe, print)
+            IntCodeComputer(opcodes, self.recv_from_pipe, output)
         ]
 
-    def recv_from_pipe(self):
+    def initial_input(self):
         if self.input_number == 0:
             self.input_number += 1
-            value = input('Enter input: ')
-            if DEBUG:
-                print(f'outputted {value} from user')
+            value = self.phase_settings[self.phase_index]
+            self.phase_index += 1
+            return value
+        else:
+            return 0
+
+    def recv_from_pipe(self):
+        # Receive phase setting first, then regular input
+        if self.input_number == 0:
+            self.input_number += 1
+            value = self.phase_settings[self.phase_index]
+            self.phase_index += 1
             return value
         else:
             if DEBUG:
