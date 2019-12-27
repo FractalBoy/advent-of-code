edcb59851fe3de3ae48b7596c307041f1f894365
Marc Reisner <reisner.marc@gmail.com>
2019-12-27 15:47:39 -0600

    day 10 part 2

--- map.py
+++ map.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
-from math import sqrt, atan2, cos, sin
+from math import sqrt, atan2, cos, sin, pi
+from itertools import groupby
 import fileinput
 
 
 def main():
     map = Map(line.strip() for line in fileinput.input())
     print(map.find_best_monitoring_location())
+    print(map.vaporize_asteroids(200))
 
 
 class Map:
@@ -15,7 +17,7 @@ class Map:
                     for x, char in enumerate(line)}
 
     def find_best_monitoring_location(self):
-        return max({
+        best_monitoring_location = max({
             origin: len(
                 set(
                     Point.from_cartesian(
@@ -26,8 +28,71 @@ class Map:
             for origin in self.asteroids()
         }.items(), key=lambda item: item[1])
 
+        self.best_monitoring_location = best_monitoring_location[0]
+        return best_monitoring_location
+
+    def vaporize_asteroids(self, vaporized_count):
+        if self.best_monitoring_location is None:
+            self.find_best_monitoring_location()
+        
+        # Get data about all points using the best monitoring location
+        # as the origin of the coordinate system.
+        points = {
+            asteroid: Point.from_cartesian(
+                asteroid[0] - self.best_monitoring_location[0],
+                asteroid[1] - self.best_monitoring_location[1]
+            )
+            for asteroid in self.asteroids()
+            # Ignore the monitoring station
+            if asteroid != self.best_monitoring_location
+        }
+
+        number = 0
+
+        while len(points):
+            def quadrant_lambda(item): return item[1].quadrant()
+            def theta_lambda(item): return item[1].theta
+            def r_lambda(item): return item[1].r
+
+            # Create dictionary of form:
+            # {
+            #   quadrant: {
+            #       angle: [list of points on that angle, sorted by distance from origin]
+            #   }
+            # }
+            quadrants = {
+                quadrant: {
+                    angle:
+                    sorted((asteroid for asteroid in angle_group),
+                           key=r_lambda)
+                    for angle, angle_group in
+                    groupby(sorted(quadrant_group, key=theta_lambda),
+                            key=theta_lambda)
+                }
+                for quadrant, quadrant_group in
+                groupby(sorted(points.items(), key=quadrant_lambda),
+                        key=quadrant_lambda)
+            }
+
+            # Clockwise starting at 12 o'clock means going through quadrants in order: 1, 4, 3, 2.
+            # Within each quadrant, look at angles in decreasing order
+            for quadrant in 1, 4, 3, 2:
+                if quadrant not in quadrants:
+                    continue
+                quadrant_group = quadrants[quadrant]
+                # Sort by angle (the key of the quadrant_group dictionary)
+                for _, asteroids in sorted(quadrant_group.items(), key=lambda item: item[0], reverse=True):
+                    closest_asteroid, _ = asteroids[0]
+                    del points[closest_asteroid]
+                    number += 1
+                    if number == vaporized_count:
+                        return closest_asteroid
+
+        return None
+
     def asteroids(self):
         return (coord for coord, value in self.map.items() if value == '#')
+
 
 class Point():
     @classmethod
@@ -46,13 +111,26 @@ class Point():
         point._calculate_cartesian()
         return point
 
+    def quadrant(self):
+        if self.theta >= 0 and self.theta <= pi/2:
+            return 1
+        if self.theta > pi/2 and self.theta <= pi:
+            return 2
+        if self.theta >= -pi and self.theta < -pi/2:
+            return 3
+        if self.theta >= -pi/2 and self.theta < 0:
+            return 4
+
     def _calculate_cartesian(self):
         self.x = self.r * cos(self.theta)
         self.y = self.r * sin(self.theta)
 
     def _calculate_polar(self):
         self.r = sqrt(self.x * self.x + self.y * self.y)
-        self.theta = atan2(self.y, self.x)
+        # Negative atan2 because the lower the y coordinate
+        # the higher the coordinate in actual space
+        self.theta = -atan2(self.y, self.x)
+        self.theta = 0 if self.theta == -0 else self.theta
 
 
 if __name__ == '__main__':
